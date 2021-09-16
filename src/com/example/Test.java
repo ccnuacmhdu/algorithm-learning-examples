18c4110b19dbb8b3e087aa2f6981560b1342adce
ccnuacmhdu <ccnuacmhdu@ccnuacmhdu.com>
2021-09-16 13:08:02 +0800

    Leetcode_102

--- src/com/example/Test.java
+++ src/com/example/Test.java
@@ -35,28 +35,30 @@ class TreeNode {
 }
 
 class Solution {
-    /**
-     * 一棵树是否是对称的，指的是左树和右树是否对称，取决于，
-     * 1）左树根和右树根相等
-     * 2）左树的左孩子和右树的右孩子是否对称
-     * 3）左树的右孩子和右树的左孩子是否对称
-     *
-     * 重要思想！！！树 T 是否对称，不就是判定 T 和 T 是否对称的吗？！可以假定一个 dummy 树根，
-     * 其左右孩子都是 root，转化成判定 dummy 是否对称，转化成左树 root 和 右树 root 是否对称
-     *
-     * @param root
-     * @return
-     */
-    public boolean isSymmetric(TreeNode root) {
-        return process(root, root);
-    }
-    private boolean process(TreeNode r1, TreeNode r2) {
-        if(r1 == null && r2 == null) {
-            return true;
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
+        Map<TreeNode, Integer> high = new HashMap<>();
+        LinkedList<TreeNode> que = new LinkedList<>();
+        TreeNode t = null;
+        que.add(root);
+        high.put(root, 0);
+        while (!que.isEmpty()) {
+            t = que.poll();
+            int h = high.get(t);
+            if(h >= res.size()) {
+                res.add(new ArrayList<>());
+            }
+            res.get(h).add(t.val);
+            if(t.left != null) {
+                que.add(t.left);
+                high.put(t.left, h + 1);
+            }
+            if(t.right != null) {
+                que.add(t.right);
+                high.put(t.right, h + 1);
+            }
         }
-        if(r1 == null || r2 == null) {
-            return false;
-        }
-        return r1.val == r2.val && process(r1.left, r2.right) && process(r1.right, r2.left);
+        return res;
     }
 }
