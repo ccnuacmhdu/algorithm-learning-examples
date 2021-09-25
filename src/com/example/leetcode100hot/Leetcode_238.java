ab648a0266af9b380dd6c61163b7195920b8b6f6
ccnuacmhdu <ccnuacmhdu@ccnuacmhdu.com>
2021-09-25 22:37:50 +0800

    Leetcode_238

---
+++ src/com/example/leetcode100hot/Leetcode_238.java
@@ -0,0 +1,16 @@
+package com.example.leetcode100hot;
+
+public class Leetcode_238 {
+    public int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+        for(int i = 1; i < nums.length; i++) {
+            output[i] = i == 1 ? output[1] = nums[0] : output[i-1] * nums[i-1];
+        }
+        int c = 1;
+        for(int i = nums.length - 2; i >= 0; i--) {
+            c *= nums[i+1];
+            output[i] = i == 0 ? c : output[i] * c;
+        }
+        return output;
+    }
+}
