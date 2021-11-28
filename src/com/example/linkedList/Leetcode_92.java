1afcd3bcc424a2ce8598840464580e05ebdfd464
ccnuacmhdu <ccnuacmhdu@ccnuacmhdu.com>
2021-11-28 20:28:31 +0800

    modify Leetcode_92

--- src/com/example/linkedList/Leetcode_92.java
+++ src/com/example/linkedList/Leetcode_92.java
@@ -9,34 +9,73 @@ public class Leetcode_92 {
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
+    // v2
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode t = head, next = null, pre = null;
-        ListNode finalPre = null, finalRight = null;
-        int loc = 1;
-        while (t != null) {
-            if(loc == left) {
-                finalPre = pre;
-                finalRight = t;
-                while (loc != right) {
-                    next = t.next;
-                    t.next = pre;
-                    pre = t;
-                    t = next;
-                    loc++;
-                }
-                next = t.next;
-                t.next = pre;
-                finalRight.next = next;
-                if(finalPre == null) return t;  //
-                finalPre.next = t;
-                break;  //
-            } else {
-                next = t.next;
-                pre = t;
-                t = next;
-                loc++;
-            }
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode rightNode = null;
+        ListNode rightNextNode = null;
+        ListNode leftPreNode = dummy;
+
+        for(int i = 1; i < left; i++) {
+            leftPreNode = leftPreNode.next;
         }
-        return head;
+
+        rightNode = leftPreNode;
+        for(int i = 0; i < right - left + 1; i++) {
+            rightNode = rightNode.next;
+        }
+        rightNextNode = rightNode.next;
+
+        rightNode.next = null;
+        reverseLinkedList(leftPreNode.next);
+
+        leftPreNode.next.next = rightNextNode;
+        leftPreNode.next = rightNode;
+
+        return dummy.next;
     }
+    private void reverseLinkedList(ListNode root) {
+        ListNode cur = root;
+        ListNode pre = null;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+    }
+
+    // v1
+//    public ListNode reverseBetween(ListNode head, int left, int right) {
+//        ListNode t = head, next = null, pre = null;
+//        ListNode finalPre = null, finalRight = null;
+//        int loc = 1;
+//        while (t != null) {
+//            if(loc == left) {
+//                finalPre = pre;
+//                finalRight = t;
+//                while (loc != right) {
+//                    next = t.next;
+//                    t.next = pre;
+//                    pre = t;
+//                    t = next;
+//                    loc++;
+//                }
+//                next = t.next;
+//                t.next = pre;
+//                finalRight.next = next;
+//                if(finalPre == null) return t;  //
+//                finalPre.next = t;
+//                break;  //
+//            } else {
+//                next = t.next;
+//                pre = t;
+//                t = next;
+//                loc++;
+//            }
+//        }
+//        return head;
+//    }
 }
