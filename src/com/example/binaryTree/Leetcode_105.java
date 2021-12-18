dc7d50f4c0e260a91cd986257ae6029358dffbfb
ccnuacmhdu <ccnuacmhdu@ccnuacmhdu.com>
2021-12-18 20:24:31 +0800

    Leetcode_105 modify

--- src/com/example/binaryTree/Leetcode_105.java
+++ src/com/example/binaryTree/Leetcode_105.java
@@ -18,22 +18,21 @@ public class Leetcode_105 {
     // 1 2 3 4
     // 3 2 1 4
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return process(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        buildIndexMap(inorder, indexMap);
+        return process(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, indexMap);
     }
-    private TreeNode process(int[] preorder, int preSt, int preEn, int[] inorder, int inSt, int inEn) {
+    private TreeNode process(int[] preorder, int preSt, int preEn, int[] inorder, int inSt, int inEn, Map<Integer, Integer> indexMap) {
         if(preSt > preEn) return null;
         TreeNode r = new TreeNode(preorder[preSt]);
-        int loc = getLoc(inorder, preorder[preSt], inSt, inEn);
-        r.left = process(preorder, preSt + 1, preSt + loc - inSt, inorder, inSt, loc - 1);
-        r.right = process(preorder, preSt + loc - inSt + 1, preEn, inorder, loc + 1, inEn);
+        int loc = indexMap.get(preorder[preSt]);    // 提前存Map，快速查找
+        r.left = process(preorder, preSt + 1, preSt + loc - inSt, inorder, inSt, loc - 1, indexMap);
+        r.right = process(preorder, preSt + loc - inSt + 1, preEn, inorder, loc + 1, inEn, indexMap);
         return r;
     }
-    private int getLoc(int[] a, int v, int st, int en) {
-        for(int i = st; i <= en; i++) {
-            if(a[i] == v) {
-                return i;
-            }
+    private void buildIndexMap(int[] a, Map<Integer, Integer> map) {
+        for(int i = 0; i < a.length; i++) {
+            map.put(a[i], i);
         }
-        return -1;
     }
 }
