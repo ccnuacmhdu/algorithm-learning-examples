f2e87e18b583869817c25f9fbbb6d1db3b124286
20211220 <a20211220@20211220s-MacBook-Pro.local>
2021-12-25 23:20:28 +0800

    Leetcode_122, modify

--- src/com/example/dynamicProgramming/Leetcode_122.java
+++ src/com/example/dynamicProgramming/Leetcode_122.java
@@ -20,6 +20,22 @@ public class Leetcode_122 {
         return dp[n - 1][0];
     }
 
+//    public int maxProfit(int[] prices) {
+//        if(prices == null || prices.length < 2) {
+//            return 0;
+//        }
+//        int n = prices.length;
+//        int a = 0, b = -prices[0];
+//        int ta, tb;
+//        for(int i = 1; i < n; i++) {
+//            ta = Math.max(a, b + prices[i]);    // 用 ?: 代替 Math.max 可以加速
+//            tb = Math.max(b, a - prices[i]);
+//            a = ta;
+//            b = tb;
+//        }
+//        return a;
+//    }
+
     // 贪心，例子 1 2 3 的利润 = 3 - 1 = (2 - 1) + (3 - 2)，后种算法累加贡献度，与前种算法效果一样
 //    public int maxProfit(int[] prices) {
 //        int ans = 0;
