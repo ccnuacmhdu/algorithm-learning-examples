fe0197309e9463087f11a5ef6a4136ba0a739ae2
20211220 <a20211220@20211220s-MacBook-Pro.local>
2021-12-27 01:00:26 +0800

    Leetcode_416

--- src/com/example/dynamicProgramming/Leetcode_416.java
+++ src/com/example/dynamicProgramming/Leetcode_416.java
@@ -12,7 +12,7 @@ public class Leetcode_416 {
 //    // 01背包（暴力），[0, i] 随意取，能否恰好填满v
 //    private boolean zero1pack(int v, int[] volume, int i) {
 //        if(i < 0) return false;
-//        if(v == volume[i]) return true;
+//        if(v == 0) return true;
 //        if(v < volume[i]) {
 //            return zero1pack(v, volume, i - 1);
 //        }
@@ -23,20 +23,51 @@ public class Leetcode_416 {
     // 01背包（动态规划）
     private boolean zero1pack(int v, int[] volume) {
         int n = volume.length;
-        boolean[][] dp = new boolean[v+1][n];
-        for(int i = 1; i <= v; i++) {
-            dp[i][0] = (volume[0] == v);
-            for(int j = 1; j < n; j++) {
-                if(i == volume[j]) {
-                    dp[i][j] = true;
-                } else if(i < volume[j]) {
-                    dp[i][j] = dp[i][j-1];
-                } else {
-                    dp[i][j] = dp[i][j-1] || dp[i-volume[j]][j-1];
+        boolean[][] dp = new boolean[n][v+1];
+        for(int j = 0; j <= v; j++) {
+            dp[0][j] = (j == volume[0]) ? true : false;
+        }
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j <= v; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if(j >= volume[i]) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - volume[i]];
                 }
             }
         }
-        return dp[v][n-1];
+        return dp[n - 1][v];
     }
 
+    // 记忆化搜索
+//    public boolean canPartition(int[] nums) {
+//        int s = 0;
+//        for(int i : nums) {
+//            s += i;
+//        }
+//        if(s % 2 != 0) {
+//            return false;
+//        }
+//        s /= 2;
+//        int[][] dp = new int[nums.length][s + 1];
+//        return process(nums, 0, s, dp);
+//    }
+//    private boolean process(int[] nums, int i, int cur, int[][] dp) {
+//        if(i >= nums.length) {
+//            return cur == 0 ? true : false;
+//        }
+//        if(cur < 0) {
+//            return false;
+//        }
+//        if(dp[i][cur] != 0) {
+//            return dp[i][cur] == 1 ? true : false;
+//        }
+//        if(cur == 0) {
+//            dp[i][cur] = 1;
+//            return true;
+//        }
+//
+//        boolean ans = process(nums, i + 1, cur - nums[i], dp) || process(nums, i + 1, cur, dp);
+//        dp[i][cur] = ans ? 1 : 2;
+//        return ans;
+//    }
 }
