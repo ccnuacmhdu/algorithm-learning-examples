3e5f8bde007783870981d691fbce38e4f8bc4ff8
o <a20211220@TL5A84JNXA5O5KB.mioffice.cn>
2022-02-24 17:55:22 +0800

    modify Leetcode_3

--- src/com/example/twoPointers/Leetcode_3.java
+++ src/com/example/twoPointers/Leetcode_3.java
@@ -4,25 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Leetcode_3 {
-    public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int i = 0, j = 0;
-
+    /**
+     * 双指针，需要根据测试样例调试，难考虑全面
+     *
+     * 样例：au, pwwkew, tmmzuxt
+     *
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring(String s) {
+        if(s == null || s.length() == 0) return 0;
+        int i = 0, j = 1;
+        char[] cs = s.toCharArray();
         Map<Character, Integer> map = new HashMap<>();
-
-        int ans = 0;
-        while(i < n && j < n) {
-            char c = s.charAt(j);
-            if((!map.containsKey(c)) || (map.containsKey(c) && map.get(c) < i)) {
-                map.put(c, j);
-            } else {
+        map.put(cs[0], 0);
+        int ans = 1;
+        while (j < cs.length) {
+            if(map.containsKey(cs[j]) && map.get(cs[j]) >= i) {
                 ans = Math.max(ans, j - i);
-                i = map.get(c) + 1;
-                map.put(c, j);
+                i = map.get(cs[j]) + 1;
+            } else {
+                ans = Math.max(ans, j - i + 1);
             }
+            map.put(cs[j], j);
             j++;
         }
-        ans = Math.max(ans, j - i);
         return ans;
     }
 }
