9bd701afd95f39f2dbb5f237a0687d0e7bf52800
o <a20211220@20211220s-MacBook-Pro.local>
2022-03-26 21:27:05 +0800

    HJ105

---
+++ src/com/example/hj/HJ105.java
@@ -0,0 +1,27 @@
+package com.example.hj;
+
+import java.util.*;
+
+public class HJ105 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = 0;
+        int negN = 0;
+        int posSum = 0;
+        double posAvg = 0.0;
+        while(scanner.hasNext()) {
+            int x = scanner.nextInt();
+            n++;
+            if(x < 0) {
+                negN++;
+            } else {
+                posSum += x;
+            }
+        }
+        if(negN < n) {
+            posAvg = posSum * 1.0 / (n - negN);
+        }
+        System.out.println(negN);
+        System.out.printf("%.1f\n", posAvg);
+    }
+}
