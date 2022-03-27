4239e42e9e1ba42e93ec7792dd2fc7976ccc361d
o <a20211220@20211220s-MacBook-Pro.local>
2022-03-27 17:25:39 +0800

    HJ30 字符串合并处理

--- src/com/example/hj/HJ30.java
+++ src/com/example/hj/HJ30.java
@@ -1,8 +1,11 @@
-package com.example;
+package com.example.hj;
 
 import java.util.*;
 
-public class Main {
+/**
+ * HJ30 字符串合并处理
+ */
+public class HJ30 {
 
     private static char[] hex = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
     private static char[] biRHex = getBiR();
@@ -89,41 +92,4 @@ public class Main {
         return map;
     }
 
-}
-
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
 }
