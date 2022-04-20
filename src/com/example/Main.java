cc20a9dbaa7ac4d7a78a1c1795e36312421707af
o <a20211220@20211220s-MacBook-Pro.local>
2022-04-20 14:31:39 +0800

    modify Leetcode_33.java

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -4,11 +4,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] a = {3,2,1,5,6,4};
-        int k = 2;
 
     }
-
 }
 
 class ListNode {
--- src/com/example/binarySearch/Leetcode_33.java
+++ src/com/example/binarySearch/Leetcode_33.java
@@ -30,4 +30,57 @@ public class Leetcode_33 {
         }
         return -1;
     }
+
+
+    /**
+     * 正常二分思路，二分方式找到两段有序的再分别二分
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+//    public int search(int[] nums, int target) {
+//        int l1 = 0;
+//        int r1 = biBigR(nums, nums[0]);
+//        int l2 = r1 + 1;
+//        int r2 = nums.length - 1;
+//        int ret1 = bi(nums, target, l1, r1);
+//        if(ret1 != -1) {
+//            return ret1;
+//        }
+//        int ret2 = bi(nums, target, l2, r2);
+//        if(ret2 != -1) {
+//            return ret2;
+//        }
+//        return -1;
+//    }
+//    private int bi(int[] a, int v, int l, int r) {
+//        int loc = -1;
+//        while(l <= r) {
+//            int mid = l + ((r - l) >> 1);
+//            if(a[mid] == v) {
+//                return mid;
+//            } else if(a[mid] > v){
+//                r = mid - 1;
+//            } else {
+//                l = mid + 1;
+//            }
+//        }
+//        return loc;
+//    }
+//    private int biBigR(int[] a, int v) {
+//        int l = 0, r = a.length - 1;
+//        int loc = 0;
+//        while(l <= r) {
+//            int mid = l + ((r - l) >> 1);
+//            if(a[mid] >= v) {
+//                loc = mid;
+//                l = mid + 1;
+//            } else {
+//                r = mid - 1;
+//            }
+//        }
+//        return loc;
+//    }
+
 }
